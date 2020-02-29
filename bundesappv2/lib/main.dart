70f96e81decd811e849f544abd05bd57aa51cae1
nivando <sidvandoni@gmail.com>
2020-02-29 13:30:40 -0300

    TEMA PRETO BONITO E CHEROSO ADDED

--- bundesappv2/lib/main.dart
+++ bundesappv2/lib/main.dart
@@ -23,8 +23,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Bundoes app (beta)',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
+      theme: new ThemeData(
+        brightness: Brightness.dark,
         primarySwatch: Colors.blue,
+        primaryColor: const Color(0xFF212121),
+        accentColor: const Color(0xFF64ffda),
+        canvasColor: const Color(0xFF303030),
       ),
       home: HomePage(),
     );
@@ -49,40 +53,138 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Testanto dados do primeiro colocado"),
+        title: Text("BUNDOES APP HEUEHEU"),
       ),
       body: Container(
-        child: Center(
-          child: FutureBuilder<Classificacao>(
-            builder: (context, snapshot) {
-              if (snapshot.hasData) {
-                return ListView.builder(
-                    itemCount: snapshot.data.clube.length,
-                    itemBuilder: (context, index) {
-                      Clube times = snapshot.data.clube[index];
-                      return Column(
-                        children: <Text>[
-                          Text(
-                              'pos : ' +
-                                  times.posicao.toString() +
-                                  ' ' +
-                                  times.time.toString() +
-                                  '\n\n',
-                              style: TextStyle(fontSize: 20.0))
-                        ],
-                      );
-                    });
-              } else if (snapshot.hasError) {
-                return Text("${snapshot.error}");
-              }
+        child: FutureBuilder<Classificacao>(
+          builder: (context, snapshot) {
+            if (snapshot.hasData) {
+              Clube time = snapshot.data.clube.first;
+              // print(time.logoUrl.toString());
+              return Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    new Image.network(
+                      time.logoUrl,
+                      fit: BoxFit.fill,
+                      width: 47.0,
+                      height: 47.0,
+                    ),
+                    new Column(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        mainAxisSize: MainAxisSize.min,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          new Text(
+                            time.time,
+                            style: new TextStyle(
+                                fontSize: 12.0,
+                                color: const Color(0xFFffffff),
+                                fontWeight: FontWeight.w900,
+                                fontFamily: "Roboto"),
+                          ),
+                          new Text(
+                            ' posição : ' +
+                                time.posicao +
+                                ' , ' +
+                                time.pontos +
+                                ' pontos, ' +
+                                ' aproveitamento ' +
+                                time.aproveitamento +
+                                ' % ',
+                            style: new TextStyle(
+                                fontSize: 12.0,
+                                color: const Color(0xFFffffff),
+                                fontWeight: FontWeight.w400,
+                                fontFamily: "Roboto"),
+                          ),
+                          new Text(
+                            "Extras",
+                            style: new TextStyle(
+                                fontSize: 12.0,
+                                color: const Color(0xFFffffff),
+                                fontWeight: FontWeight.w200,
+                                fontFamily: "Roboto"),
+                          )
+                        ])
+                  ]);
+            } else if (snapshot.hasError) {
+              return Text("${snapshot.error}");
+            }
 
-              // By default, show a loading spinner.
-              return CircularProgressIndicator();
-            },
-            future: futureClassificacao,
-          ),
+            // By default, show a loading spinner.
+            return CircularProgressIndicator();
+          },
+          future: futureClassificacao,
         ),
       ),
     );
   }
 }
+/*
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key}) : super(key: key);
+
+  @override
+  _MyHomePageState createState() => new _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  Future<Classificacao> futureClassificacao;
+
+  void fabPressed() {}
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('BUNDOES APP HUEHEUEH'),
+      ),
+      body: new Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          mainAxisSize: MainAxisSize.max,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            new Image.network(
+              'https://github.com/flutter/website/blob/master/_includes/code/layout/lakes/images/lake.jpg?raw=true',
+              fit: BoxFit.fill,
+              width: 67.0,
+              height: 67.0,
+            ),
+            Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  new Text(
+                    "Nome do time",
+                    style: new TextStyle(
+                        fontSize: 18.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Roboto"),
+                  ),
+                  new Text(
+                    "campeonato x - posição x, x pontos, x% de aproveitamento",
+                    style: new TextStyle(
+                        fontSize: 8.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Roboto"),
+                  ),
+                  new Text(
+                    "último resultado vitória por 1- 0 contra time y ",
+                    style: new TextStyle(
+                        fontSize: 8.0,
+                        color: const Color(0xFF000000),
+                        fontWeight: FontWeight.w500,
+                        fontFamily: "Roboto"),
+                  )
+                ])
+          ]),
+    );
+  }
+}
+*/
